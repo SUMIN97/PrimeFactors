86131eaf5d6f73cbdd38088bdfab796d0af88b2c
sumin1s.song <sumin1s.song@samsung.com>
2024-07-10 10:08:22 +1000

    [refactor] pg36

--- prime_factor.py
+++ prime_factor.py
@@ -1,12 +1,15 @@
-
-
 class PrimeFactor:
     def of(self, number) -> []:
         factors = []
         if number > 1:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                if number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
+                if number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
+
             else:
                 factors.append(number)
         return factors
